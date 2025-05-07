f0be76d2d9ab42a28ef943c508ae73f7978a4813
Emiliano Flores <jemilianofl@gmail.com>
2025-05-07 15:31:35 -0600

    Added: Servidor y rutas de la API

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+
+const userRoutes = require('./src/routes/userRoutes');
+const taskRoutes = require('./src/routes/taskRoutes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/users', userRoutes);
+app.use('/api/tasks', taskRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+});
